4fdaf8ae3b8df799c6fff6124e1784f5e26ed67f
Julie✦ <12161851+JulieAnneTran@users.noreply.github.com>
2021-08-15 02:13:54 +1000

    Cleaned up code.

--- src/Components/BackgroundVideo.jsx
+++ src/Components/BackgroundVideo.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
+import React from 'react';
 
-const BackgroundVideo = ({ videoSource, children }) => {
+const BackgroundVideo = ({ videoSource }) => {
     return (
         <>
-        <div className='container'>
-            <video
-            autoPlay="autoplay"
-            muted
-            id="video-id"
-            className='video' >
-            <source src={videoSource} type="video/mp4" />
-                Your browser does not support the video tag.
-        </video>
-        </div>
+            <div className='container'>
+                <video
+                    autoPlay="autoplay"
+                    muted
+                    id="video"
+                    className='video' >
+                    <source src={videoSource} type="video/mp4" />
+                    Your browser does not support the video tag.
+            </video>
+            </div>
         </>
     )
 }
 
-export default BackgroundVideo
+export default BackgroundVideo;
